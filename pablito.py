21254a4b69df1a1165f62080b77f894254b82bc6
karlamagueta <k.magueta@gmail.com>
2020-05-09 02:37:28 +0000

    v1.2 pablito as avessas

--- pablito.py
+++ pablito.py
@@ -1,16 +1,15 @@
 resposta = input("Pablo, qual é a musica?")
+resposta = resposta.strip()
 print(resposta)
 
-# input
+
 idade = input("Pablo, qual a sua idade")
 
-# validacao
 idade = idade.strip()
-if not idade.isnumeric():
-    print("errou feio, errou rude")
-else:
-    # computacao / processamento
-    ano_que_vem = int(idade) + 1
+if idade.isnumeric():
+    ano_que_vem  = int(idade)+1
+    print("minha idade ano que vem será..... hehehe ",ano_que_vem)
 
-    # output
-    print("minha idade ano que bem é", ano_que_vem)
+else:
+    print("errou feio, errou rude")
+
