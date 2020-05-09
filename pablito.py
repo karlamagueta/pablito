451a1625296e4a5e654387180e55bd0fe30cb23a
karlamagueta <k.magueta@gmail.com>
2020-05-09 03:04:17 +0000

    v1.3 pablito as avessas ++

--- pablito.py
+++ pablito.py
@@ -7,9 +7,13 @@ idade = input("Pablo, qual a sua idade")
 
 idade = idade.strip()
 if idade.isnumeric():
-    ano_que_vem  = int(idade)+1
-    print("minha idade ano que vem será..... hehehe ",ano_que_vem)
+    ano_que_vem  = int(idade) + 1
+    ano_que_que_vem = int(idade) + 2
+    print(
+        f"minha idade ano que vem será..... hehehe {ano_que_vem} "
+        f"e ano que que vem... {ano_que_que_vem} "
+        "por quê separado se escreve junto e tudo junto se escreve separado?"
+    )
 
 else:
     print("errou feio, errou rude")
-
